d4d99c322bc5bbaa75b7e26b04b518706f106d51
juanvv-alt <juanvv@gmail.com>
2026-06-08 16:55:15 +0800

    fix: add pytest.ini and test_settings.py so pytest runs without explicit DATABASE_ENGINE env var

--- conftest.py
+++ conftest.py
@@ -1,9 +1,3 @@
 import os
 
-import django
-from django.conf import settings
-
-
-def pytest_configure(config):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-    os.environ.setdefault("DATABASE_ENGINE", "django.db.backends.sqlite3")
+os.environ.setdefault("DATABASE_ENGINE", "django.db.backends.sqlite3")
